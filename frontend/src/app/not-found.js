857afa717f35cc66ac36eb743a7203dd4d1f89f0
giftormdh <giftoramadha@gmail.com>
2024-08-05 23:12:07 +0700

    add: 404 not found page styling

---
+++ frontend/src/app/not-found.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <Image src="/404.svg" alt="404 Page Not Found" width={400} height={400} />
+        <Link href="/">
+          <button>Go Back Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
