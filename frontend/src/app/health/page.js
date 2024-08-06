cbe7c1003354a02426c7308ee7a65c749b56204a
giftormdh <giftoramadha@gmail.com>
2024-08-07 02:34:46 +0700

    feat: health services page

---
+++ frontend/src/app/health/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const HealthPage = () => {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Mobile view */}
+      <div className="block sm:hidden w-full h-full overflow-y-auto">
+        <div className="relative w-full h-[200vh]">
+          <Image src="/health-corner-portrait.svg" alt="Health Corner Portrait" layout="fill" objectFit="cover" />
+        </div>
+      </div>
+      {/* Web view */}
+      <div className="hidden sm:block w-full h-full">
+        <Image src="/health-corner.svg" alt="Health Corner" layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  );
+};
+
+export default HealthPage;
