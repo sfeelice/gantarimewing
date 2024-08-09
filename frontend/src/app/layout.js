12041354c8094b9b0fc7c616778f3fb57cd27f08
giftormdh <giftoramadha@gmail.com>
2024-08-10 03:17:55 +0700

    feat: navbar exception for all admin's page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,12 +3,14 @@ import { Urbanist } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation"; // Import usePathname
 import "./globals.css";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   const [scroll, setScroll] = useState(false);
+  const pathname = usePathname(); // Inisialisasi usePathname
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,32 +26,38 @@ export default function RootLayout({ children }) {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // Daftar path di mana navbar tidak akan ditampilkan
+  const noNavbarPaths = ["/auth/sign-in", "/auth/sign-up", "/admin/dashboard"];
+  const showNavbar = !noNavbarPaths.includes(pathname);
+
   return (
     <html lang="en">
       <body className={urbanist.className}>
-        <header className={`fixed w-full z-30 transition-all duration-300 ${scroll ? "bg-opacity-75 bg-black shadow-lg" : "bg-transparent"}`}>
-          <nav className="container mx-auto flex items-center justify-between p-5">
-            <a href="/">
-              <div className="flex items-center">
-                <Image src="/mengwi-logo-text-white.svg" alt="Mengwi Logo" width={100} height={100} />
+        {showNavbar && (
+          <header className={`fixed w-full z-30 transition-all duration-300 ${scroll ? "bg-opacity-75 bg-black shadow-lg" : "bg-transparent"}`}>
+            <nav className="container mx-auto flex items-center justify-between p-5">
+              <Link href="/">
+                <div className="flex items-center">
+                  <Image src="/mengwi-logo-text-white.svg" alt="Mengwi Logo" width={100} height={100} />
+                </div>
+              </Link>
+              <div className={`flex items-center space-x-16 text-white text-lg`}>
+                <Link href="/baha">
+                  <div>Baha Village</div>
+                </Link>
+                <Link href="/sobangan">
+                  <div>Sobangan Village</div>
+                </Link>
+                <Link href="/health">
+                  <div>Health Services</div>
+                </Link>
               </div>
-            </a>
-            <div className={`flex items-center space-x-16 text-white text-lg`}>
-              <a href="/baha">
-                <div>Baha Village</div>
-              </a>
-              <a href="/sobangan">
-                <div>Sobangan Village</div>
-              </a>
-              <a href="/health">
-                <div>Health Services</div>
-              </a>
-            </div>
-            <a href="/contact-us">
-              <div className={`px-4 py-2 rounded-full bg-white font-semibold hover:bg-primary hover:text-white`}>Contact Us</div>
-            </a>
-          </nav>
-        </header>
+              <Link href="/contact-us">
+                <div className={`px-4 py-2 rounded-full bg-white font-semibold hover:bg-primary hover:text-white`}>Contact Us</div>
+              </Link>
+            </nav>
+          </header>
+        )}
         {children}
       </body>
     </html>
