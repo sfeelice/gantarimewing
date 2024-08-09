371e38b09e0c0479ab974a0473c307853c320397
Angelicaviera <angelica.callysta.viera@mail.ugm.ac.id>
2024-08-10 03:11:13 +0700

    add status param at signup

--- frontend/src/app/auth/sign-up/page.js
+++ frontend/src/app/auth/sign-up/page.js
@@ -11,6 +11,7 @@ export default function SignUp() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [status, setStatus] = useState(""); // Default value
   const router = useRouter();
 
   const handleSubmit = async (e) => {
@@ -20,17 +21,19 @@ export default function SignUp() {
       return;
     }
     try {
+      console.log("Submitting data:", { username, email, password, status });
       const response = await axios.post(
         "http://localhost:5000/admin/SignUp",
         {
           username,
           email,
           password,
+          status
         },
         { withCredentials: true } // If you're using cookies for authentication
       );
 
-      if (response.status === 201) {
+      if (response.status === 200 || response.status === 201) {
         // Redirect to sign-in page after successful registration
         router.push("/auth/sign-in");
       }
@@ -113,6 +116,33 @@ export default function SignUp() {
               <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute inset-y-0 right-3 flex items-center text-sm text-gray-600">
                 {showPassword ? "Hide" : "Show"}
               </button>
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">Status</label>
+            <div className="mt-2 space-x-4">
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  name="status"
+                  value="Baha"
+                  checked={status === "Baha"}
+                  onChange={(e) => setStatus(e.target.value)}
+                  className="form-radio"
+                />
+                <span className="ml-2">Baha</span>
+              </label>
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  name="status"
+                  value="Sobangan"
+                  checked={status === "Sobangan"}
+                  onChange={(e) => setStatus(e.target.value)}
+                  className="form-radio"
+                />
+                <span className="ml-2">Sobangan</span>
+              </label>
             </div>
           </div>
           <button type="submit" className="w-full px-4 py-2 font-bold text-white bg-primary rounded hover:bg-primary-dark">
