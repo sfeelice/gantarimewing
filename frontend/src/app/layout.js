76f13a78258b1368a7ce0c2cdb4617b994ba54ab
giftormdh <giftoramadha@gmail.com>
2024-08-10 03:43:17 +0700

    add: navbar hover and active state

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -43,13 +43,13 @@ export default function RootLayout({ children }) {
               </Link>
               <div className={`flex items-center space-x-16 text-white text-lg`}>
                 <Link href="/baha">
-                  <div>Baha Village</div>
+                  <div className={`px-4 py-2 rounded-full ${pathname === "/baha" ? "bg-primary text-white" : "hover:bg-white hover:text-black"}`}>Baha Village</div>
                 </Link>
                 <Link href="/sobangan">
-                  <div>Sobangan Village</div>
+                  <div className={`px-4 py-2 rounded-full ${pathname === "/sobangan" ? "bg-primary text-white" : "hover:bg-white hover:text-black"}`}>Sobangan Village</div>
                 </Link>
                 <Link href="/health">
-                  <div>Health Services</div>
+                  <div className={`px-4 py-2 rounded-full ${pathname === "/health" ? "bg-primary text-white" : "hover:bg-white hover:text-black"}`}>Health Services</div>
                 </Link>
               </div>
               <Link href="/contact-us">
