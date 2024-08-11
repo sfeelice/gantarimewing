8575e958af92364358b6d474496b00b763edeaed
giftormdh <giftoramadha@gmail.com>
2024-08-12 02:09:09 +0700

    add: content writing

--- frontend/src/app/sobangan/page.js
+++ frontend/src/app/sobangan/page.js
@@ -11,15 +11,18 @@ const Sobangan = () => {
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center">
           <div className="w-full flex justify-between mx-[10vw] gap-10">
             {/* Description */}
-            <div className="text-white">
+            <div className="text-white shadow-xl">
               <div className="font-bold text-5xl mb-6">Sobangan Village</div>
-              <div>
-                Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini Deskripsinya Ini
-                Deskripsinya Ini Deskripsinya
+              <div className="text-justify">
+                Sobangan Village, located in Mengwi District, Badung Regency, covers an area of approximately 256.56 hectares. It is bordered by Sembung Village to the north, Ayunan Village to the
+                east, Baha Village to the south, and Werdi Bhuwana Village to the west. This village consists of two traditional villages, namely Dukuh Moncos Traditional Village and Sobangan
+                Traditional Village, and includes four Banjars: Banjar Dukuh Moncos, Banjar Tegalnarungan, Banjar Tengah, and Banjar Selat. Additionally, Sobangan has four Subaks, namely Subak Babakan
+                Sobangan, Subak Abian Manik Sarwa Nadi, Subak Abian Tegal Sari, and Subak Abian Werdi Putra Sedana, highlighting the strong agricultural traditions of the village. The potential of
+                Sobangan Village spans various sectors including agriculture, education, livestock farming, human resources, arts, and MSMEs.
               </div>
             </div>
             {/* Youtube Video */}
-            <div className="h-[50vh] flex items-center justify-center bg-yellow-100">
+            <div className="h-[50vh] flex items-center my-auto justify-center bg-black">
               <iframe
                 width="560"
                 height="315"
@@ -35,7 +38,17 @@ const Sobangan = () => {
       {/* Section 2 */}
       <div className="h-screen bg-white flex flex-col items-center justify-center">
         <div className="relative w-full h-[50vh]">
-          <Image src="/pw-baha.svg" layout="fill" objectFit="contain" alt="Baha Tourism Map" priority />
+          {/* Youtube Video */}
+          <div className="h-[50vh] flex items-center justify-center bg-black">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-9d8eqEG1os"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>{" "}
         </div>
       </div>
       {/* Section 3 Village Tourism*/}
